7fe79de5ccb0a4c3170b036dd25f3716adb862cf
Maria Elizabeth Chuico Medina <166523237+mariaechm@users.noreply.github.com>
2025-02-12 20:56:34 -0500

    elimination

--- vista/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-def create_app():
-    app = Flask(__name__, instance_relative_config=False)
-    with app.app_context():
-      from routes.route import router
-      app.register_blueprint(router)
-      return app
